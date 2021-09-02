136c2beb1d18e8881c05aa7cad01e9f51a06ab9c
Joshua Brooks <josh.vdbroek@gmail.com>
2021-09-02 10:55:58 +0900

    added models and admin

--- admin.py
+++ admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import PlanetOsmLine, PlanetOsmPoint, PlanetOsmPolygon,PlanetOsmRoads
 # Register your models here.
+
+@admin.register(PlanetOsmLine)
+class PlanetOsmLineAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(PlanetOsmPoint)
+class PlanetOsmPointAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(PlanetOsmPolygon)
+class PlanetOsmPolygonAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(PlanetOsmRoads)
+class PlanetOsmRoadsAdmin(admin.ModelAdmin):
+    ...
--- models.py
+++ models.py
@@ -1,3 +1,207 @@
 from django.db import models
 
-# Create your models here.
+
+class PlanetOsmLine(models.Model):
+    unique_id = models.BigIntegerField(primary_key=True)
+    osm_id = models.BigIntegerField(blank=True, null=True)
+    access = models.TextField(blank=True, null=True)
+    addr_housename = models.TextField(db_column='addr:housename', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_housenumber = models.TextField(db_column='addr:housenumber', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_interpolation = models.TextField(db_column='addr:interpolation', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    admin_level = models.TextField(blank=True, null=True)
+    aerialway = models.TextField(blank=True, null=True)
+    aeroway = models.TextField(blank=True, null=True)
+    amenity = models.TextField(blank=True, null=True)
+    barrier = models.TextField(blank=True, null=True)
+    bicycle = models.TextField(blank=True, null=True)
+    bridge = models.TextField(blank=True, null=True)
+    boundary = models.TextField(blank=True, null=True)
+    building = models.TextField(blank=True, null=True)
+    construction = models.TextField(blank=True, null=True)
+    covered = models.TextField(blank=True, null=True)
+    foot = models.TextField(blank=True, null=True)
+    highway = models.TextField(blank=True, null=True)
+    historic = models.TextField(blank=True, null=True)
+    horse = models.TextField(blank=True, null=True)
+    junction = models.TextField(blank=True, null=True)
+    landuse = models.TextField(blank=True, null=True)
+    layer = models.IntegerField(blank=True, null=True)
+    leisure = models.TextField(blank=True, null=True)
+    lock = models.TextField(blank=True, null=True)
+    man_made = models.TextField(blank=True, null=True)
+    military = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    natural = models.TextField(blank=True, null=True)
+    oneway = models.TextField(blank=True, null=True)
+    place = models.TextField(blank=True, null=True)
+    power = models.TextField(blank=True, null=True)
+    railway = models.TextField(blank=True, null=True)
+    ref = models.TextField(blank=True, null=True)
+    religion = models.TextField(blank=True, null=True)
+    route = models.TextField(blank=True, null=True)
+    service = models.TextField(blank=True, null=True)
+    shop = models.TextField(blank=True, null=True)
+    surface = models.TextField(blank=True, null=True)
+    tourism = models.TextField(blank=True, null=True)
+    tracktype = models.TextField(blank=True, null=True)
+    tunnel = models.TextField(blank=True, null=True)
+    water = models.TextField(blank=True, null=True)
+    waterway = models.TextField(blank=True, null=True)
+    way_area = models.FloatField(blank=True, null=True)
+    z_order = models.IntegerField(blank=True, null=True)
+    way = models.GeometryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'planet_osm_line'
+
+
+class PlanetOsmPoint(models.Model):
+    unique_id = models.BigIntegerField(primary_key=True)
+    osm_id = models.BigIntegerField(blank=True, null=True)
+    access = models.TextField(blank=True, null=True)
+    addr_housename = models.TextField(db_column='addr:housename', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_housenumber = models.TextField(db_column='addr:housenumber', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    admin_level = models.TextField(blank=True, null=True)
+    aerialway = models.TextField(blank=True, null=True)
+    aeroway = models.TextField(blank=True, null=True)
+    amenity = models.TextField(blank=True, null=True)
+    barrier = models.TextField(blank=True, null=True)
+    boundary = models.TextField(blank=True, null=True)
+    building = models.TextField(blank=True, null=True)
+    highway = models.TextField(blank=True, null=True)
+    historic = models.TextField(blank=True, null=True)
+    junction = models.TextField(blank=True, null=True)
+    landuse = models.TextField(blank=True, null=True)
+    layer = models.IntegerField(blank=True, null=True)
+    leisure = models.TextField(blank=True, null=True)
+    lock = models.TextField(blank=True, null=True)
+    man_made = models.TextField(blank=True, null=True)
+    military = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    natural = models.TextField(blank=True, null=True)
+    oneway = models.TextField(blank=True, null=True)
+    place = models.TextField(blank=True, null=True)
+    power = models.TextField(blank=True, null=True)
+    railway = models.TextField(blank=True, null=True)
+    ref = models.TextField(blank=True, null=True)
+    religion = models.TextField(blank=True, null=True)
+    shop = models.TextField(blank=True, null=True)
+    tourism = models.TextField(blank=True, null=True)
+    water = models.TextField(blank=True, null=True)
+    waterway = models.TextField(blank=True, null=True)
+    way = models.GeometryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'planet_osm_point'
+
+
+class PlanetOsmPolygon(models.Model):
+    unique_id = models.BigIntegerField(primary_key=True)
+    osm_id = models.BigIntegerField(blank=True, null=True)
+    access = models.TextField(blank=True, null=True)
+    addr_housename = models.TextField(db_column='addr:housename', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_housenumber = models.TextField(db_column='addr:housenumber', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_interpolation = models.TextField(db_column='addr:interpolation', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    admin_level = models.TextField(blank=True, null=True)
+    aerialway = models.TextField(blank=True, null=True)
+    aeroway = models.TextField(blank=True, null=True)
+    amenity = models.TextField(blank=True, null=True)
+    barrier = models.TextField(blank=True, null=True)
+    bicycle = models.TextField(blank=True, null=True)
+    bridge = models.TextField(blank=True, null=True)
+    boundary = models.TextField(blank=True, null=True)
+    building = models.TextField(blank=True, null=True)
+    construction = models.TextField(blank=True, null=True)
+    covered = models.TextField(blank=True, null=True)
+    foot = models.TextField(blank=True, null=True)
+    highway = models.TextField(blank=True, null=True)
+    historic = models.TextField(blank=True, null=True)
+    horse = models.TextField(blank=True, null=True)
+    junction = models.TextField(blank=True, null=True)
+    landuse = models.TextField(blank=True, null=True)
+    layer = models.IntegerField(blank=True, null=True)
+    leisure = models.TextField(blank=True, null=True)
+    lock = models.TextField(blank=True, null=True)
+    man_made = models.TextField(blank=True, null=True)
+    military = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    natural = models.TextField(blank=True, null=True)
+    oneway = models.TextField(blank=True, null=True)
+    place = models.TextField(blank=True, null=True)
+    power = models.TextField(blank=True, null=True)
+    railway = models.TextField(blank=True, null=True)
+    ref = models.TextField(blank=True, null=True)
+    religion = models.TextField(blank=True, null=True)
+    route = models.TextField(blank=True, null=True)
+    service = models.TextField(blank=True, null=True)
+    shop = models.TextField(blank=True, null=True)
+    surface = models.TextField(blank=True, null=True)
+    tourism = models.TextField(blank=True, null=True)
+    tracktype = models.TextField(blank=True, null=True)
+    tunnel = models.TextField(blank=True, null=True)
+    water = models.TextField(blank=True, null=True)
+    waterway = models.TextField(blank=True, null=True)
+    way_area = models.FloatField(blank=True, null=True)
+    z_order = models.IntegerField(blank=True, null=True)
+    way = models.GeometryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'planet_osm_polygon'
+
+
+class PlanetOsmRoads(models.Model):
+    unique_id = models.BigIntegerField(primary_key=True)
+    osm_id = models.BigIntegerField(blank=True, null=True)
+    access = models.TextField(blank=True, null=True)
+    addr_housename = models.TextField(db_column='addr:housename', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_housenumber = models.TextField(db_column='addr:housenumber', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    addr_interpolation = models.TextField(db_column='addr:interpolation', blank=True, null=True)  # Field renamed to remove unsuitable characters.
+    admin_level = models.TextField(blank=True, null=True)
+    aerialway = models.TextField(blank=True, null=True)
+    aeroway = models.TextField(blank=True, null=True)
+    amenity = models.TextField(blank=True, null=True)
+    barrier = models.TextField(blank=True, null=True)
+    bicycle = models.TextField(blank=True, null=True)
+    bridge = models.TextField(blank=True, null=True)
+    boundary = models.TextField(blank=True, null=True)
+    building = models.TextField(blank=True, null=True)
+    construction = models.TextField(blank=True, null=True)
+    covered = models.TextField(blank=True, null=True)
+    foot = models.TextField(blank=True, null=True)
+    highway = models.TextField(blank=True, null=True)
+    historic = models.TextField(blank=True, null=True)
+    horse = models.TextField(blank=True, null=True)
+    junction = models.TextField(blank=True, null=True)
+    landuse = models.TextField(blank=True, null=True)
+    layer = models.IntegerField(blank=True, null=True)
+    leisure = models.TextField(blank=True, null=True)
+    lock = models.TextField(blank=True, null=True)
+    man_made = models.TextField(blank=True, null=True)
+    military = models.TextField(blank=True, null=True)
+    name = models.TextField(blank=True, null=True)
+    natural = models.TextField(blank=True, null=True)
+    oneway = models.TextField(blank=True, null=True)
+    place = models.TextField(blank=True, null=True)
+    power = models.TextField(blank=True, null=True)
+    railway = models.TextField(blank=True, null=True)
+    ref = models.TextField(blank=True, null=True)
+    religion = models.TextField(blank=True, null=True)
+    route = models.TextField(blank=True, null=True)
+    service = models.TextField(blank=True, null=True)
+    shop = models.TextField(blank=True, null=True)
+    surface = models.TextField(blank=True, null=True)
+    tourism = models.TextField(blank=True, null=True)
+    tracktype = models.TextField(blank=True, null=True)
+    tunnel = models.TextField(blank=True, null=True)
+    water = models.TextField(blank=True, null=True)
+    waterway = models.TextField(blank=True, null=True)
+    way_area = models.FloatField(blank=True, null=True)
+    z_order = models.IntegerField(blank=True, null=True)
+    way = models.GeometryField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'planet_osm_roads'
