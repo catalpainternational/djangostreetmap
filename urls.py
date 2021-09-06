5837335d75c4d8013826e5721b28e795f39e0159
Joshua Brooks <josh.vdbroek@gmail.com>
2021-09-07 01:10:20 +0900

    simpler cache in URLs

--- urls.py
+++ urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
-from . import views
+from .views import (
+    AdminBoundaryLayerView,
+    ExampleMapView,
+    IslandsAreaLayerView,
+    RoadLayerView,
+)
 
 urlpatterns = [
-    path("highways/<int:zoom>/<int:x>/<int:y>.pbf", views.RoadLayerView.as_view()),
-    path("admboundary/<int:zoom>/<int:x>/<int:y>.pbf", views.AdminBoundaryLayerView.as_view()),
-    path("islands/<int:zoom>/<int:x>/<int:y>.pbf", views.IslandsAreaLayerView.as_view()),
-    path("example_map", views.ExampleMapView.as_view()),
+    path("highways/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(RoadLayerView.as_view())),
+    path("admboundary/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(AdminBoundaryLayerView.as_view())),
+    path("islands/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(IslandsAreaLayerView.as_view())),
+    path("example_map", ExampleMapView.as_view()),
 ]
