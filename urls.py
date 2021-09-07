9b454b1a311fe3ff38bcb51b1cd9c2d428e0572d
Joshua Brooks <josh.vdbroek@gmail.com>
2021-09-07 15:35:59 +0900

    Correct srid and urls

--- urls.py
+++ urls.py
@@ -11,12 +11,12 @@ from .views import (
 app_name = "djangostreetmap"
 
 urlpatterns = [
-    path("highways/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(views.RoadLayerView.as_view()), name="highways"),
-    path("admboundary/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(views.AdminBoundaryLayerView.as_view()), name="admin"),
-    path("islands/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(views.IslandsAreaLayerView.as_view()), name="islands"),
-    path("example_map", views.ExampleMapView.as_view()),
+    path("highways/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(RoadLayerView.as_view()), name="highways"),
+    path("admboundary/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(AdminBoundaryLayerView.as_view()), name="admin"),
+    path("islands/<int:zoom>/<int:x>/<int:y>.pbf", cache_page(60 * 15)(IslandsAreaLayerView.as_view()), name="islands"),
+    path("example_map", ExampleMapView.as_view()),
     # These allow maps to make reverse URL calls a little easier
-    path("highways/", views.RoadLayerView.as_view(), name="highways"),
-    path("admboundary", views.AdminBoundaryLayerView.as_view(), name="admin"),
-    path("islands/", views.IslandsAreaLayerView.as_view(), name="islands"),
+    path("highways/", RoadLayerView.as_view(), name="highways"),
+    path("admboundary/", AdminBoundaryLayerView.as_view(), name="admin"),
+    path("islands/", IslandsAreaLayerView.as_view(), name="islands"),
 ]
