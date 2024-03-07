7f558b303c104c68ec6be226b44b4c8aad407a87
Raphael Merx <raphael.merx@gmail.com>
2024-03-07 11:16:49 +0800

    Remove usage of GeoModelAdmin
    Removed in Django 5, see https://code.djangoproject.com/ticket/27674

--- djangostreetmap/admin.py
+++ djangostreetmap/admin.py
@@ -1,8 +1,11 @@
-from django.contrib.gis import admin
+from django.contrib import admin
+from django.contrib.gis.forms import OSMWidget
 
 from djangostreetmap import models
 
 
 @admin.register(models.SimplifiedLandPolygon)
-class SimplifiedLandPolygonAdmin(admin.GeoModelAdmin):  # type: ignore
-    pass
+class SimplifiedLandPolygonAdmin(admin.ModelAdmin):  # type: ignore
+    formfield_overrides = {
+        models.MultiPolygonField: {"widget": OSMWidget},
+    }
